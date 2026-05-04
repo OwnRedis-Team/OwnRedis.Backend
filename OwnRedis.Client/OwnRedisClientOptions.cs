0e99475081faea1b95a72516600dba17a9407f12
EgorSalahov <egorsalahov2@gmail.com>
2026-05-04 16:54:13 +0400

    #62305654 Create OwnRedis.Client

---
+++ OwnRedis.Client/OwnRedisClientOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OwnRedis.Client
+{
+    public class OwnRedisClientOptions
+    {
+        public string BaseUrl { get; set; } = string.Empty;
+        public TimeSpan DefaultTtl { get; set; } = TimeSpan.FromMinutes(10);
+    }
+}
---
+++ OwnRedis.Client/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using OwnRedis.Client.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OwnRedis.Client
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddOwnRedisClient(
+            this IServiceCollection services,
+            Action<OwnRedisClientOptions> configure)
+        {
+            var options = new OwnRedisClientOptions();
+            configure(options);
+
+            services.AddSingleton(options);
+            services.AddHttpClient<IOwnRedisClient, OwnRedisClient>(client =>
+            {
+                client.BaseAddress = new Uri(options.BaseUrl);
+            });
+
+            return services;
+        }
+    }
+}
